3911294c60122908fc5f6085ecf1a752933c8ccf
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2024-07-22 20:23:26 +0530

    routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ContentBodyComponent } from './content-body/content-body.component';
+import { HomeComponent } from './home/home.component';
+import { BodyComponent } from './body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BodyComponent},
+  {path: 'content', component: ContentBodyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
