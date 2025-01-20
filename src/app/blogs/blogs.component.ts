dd1b1e6d0ef349370962177e1308a249e95a0140
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2025-01-20 16:33:56 +0530

    blogs firebase initial setup

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+
+export interface BlogResponse {
+  title: string;
+  description: string;
+  image: string;
+}
+
 
 @Component({
   selector: 'app-blogs',
@@ -7,4 +16,30 @@ import { Component } from '@angular/core';
 })
 export class BlogsComponent {
 
+  http: HttpClient = inject(HttpClient);
+  
+
+  title: string = '';
+  description: string = '';
+  image: string = '';
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.http
+      .get<BlogResponse>(
+        'https://infohub-613d6-default-rtdb.firebaseio.com/blog.json'
+      )
+      .subscribe((res) => {
+        console.log(res);
+        this.title = res.title || 'Default Title';
+        this.description = res.description || 'Default Description';
+        this.image =
+          res.image ||
+          'https://gist.github.com/assets/99909689/5f72d4b7-aaab-47c4-93c2-f19b52a8c2ac';
+      });
+  }
+  
 }
