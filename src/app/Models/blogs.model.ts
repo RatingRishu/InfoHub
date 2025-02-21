249b2bbcc1a3094ceece98e9e33226652b19a42a
ratingrishu <rairishabh5001@gmail.com>
2025-02-21 20:20:51 +0530

    blog page fixed

--- src/app/Models/blogs.model.ts
+++ src/app/Models/blogs.model.ts
@@ -1,6 +1,7 @@
 export class Blog {
   title: string = '';
   description: string = '';
+  shortDescription: string = ''; 
   image: string = '';
   id?: string;
 }
--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,7 +1,53 @@
+// import { Component, inject } from '@angular/core';
+// import { BlogService } from '../services/blog.service';
+// import { Blog } from '../Models/blogs.model';
+
+
+// @Component({
+//   selector: 'app-blogs',
+//   templateUrl: './blogs.component.html',
+//   styleUrls: ['./blogs.component.css']
+// })
+// export class BlogsComponent {
+
+  
+
+//   title: string = '';
+//   description: string = '';
+//   image: string = '';
+
+//   allBlogs: Blog[] = [];
+
+//   blogsService: BlogService = inject(BlogService);
+  
+
+//   ngOnInit(): void {
+//     this.getData();
+//   }
+
+//   getData() {
+//     this.blogsService.getAllBlogs()
+//     .subscribe({
+//       next: (blogs) => {
+//         this.allBlogs = blogs.map(blog => ({
+//           ...blog,
+//           shortDescription: blog.shortDescription || 'No short description available' // Ensure it is set
+//         }));
+//         console.log(this.allBlogs);
+//       },
+//       error: (error) => {
+//         console.log(error);
+//       }
+//     });
+//   }
+  
+// }
+
+
+
 import { Component, inject } from '@angular/core';
 import { BlogService } from '../services/blog.service';
 import { Blog } from '../Models/blogs.model';
-
 
 @Component({
   selector: 'app-blogs',
@@ -9,69 +55,38 @@ import { Blog } from '../Models/blogs.model';
   styleUrls: ['./blogs.component.css']
 })
 export class BlogsComponent {
-
-  
-
-  title: string = '';
-  description: string = '';
-  image: string = '';
-
   allBlogs: Blog[] = [];
+  selectedBlog: Blog | null = null; // To store selected blog
 
   blogsService: BlogService = inject(BlogService);
-  
 
   ngOnInit(): void {
     this.getData();
   }
 
-  // getData() {
-  //   this.http
-  //     .get<BlogResponse>(
-  //       'https://infohub-613d6-default-rtdb.firebaseio.com/blog.json'
-  //     )
-  //     .subscribe((res) => {
-  //       console.log(res);
-  //       this.title = res.title || 'Default Title';
-  //       this.description = res.description || 'Default Description';
-  //       this.image =
-  //         res.image ||
-  //         'https://gist.github.com/assets/99909689/5f72d4b7-aaab-47c4-93c2-f19b52a8c2ac';
-  //     });
-  // }
-
-  // getData() {
-  //   this.blogsService.getAllBlogs()
-  //   .subscribe((res) => {
-  //     console.log(res);
-  //     this.title = res.title || 'Default Title';
-  //     this.description = res.description || 'Default Description';
-  //     this.image =
-  //       res.image ||
-  //       'https://gist.github.com/assets/99909689/5f72d4b7-aaab-47c4-93c2-f19b52a8c2ac';
-  //   });
-  // }
-
   getData() {
     this.blogsService.getAllBlogs()
-    .subscribe({
-      next: (blog) => {
-      this.allBlogs = blog;
-      console.log(this.allBlogs);
-      // this.isLoading = false;
-
-    },
-    error: (error) => {
-      // this.setErrorMsg(error);
-      // this.isLoading = false;
-      console.log(error);
-    }
-
-    });
+      .subscribe({
+        next: (blogs) => {
+          this.allBlogs = blogs.map(blog => ({
+            ...blog,
+            shortDescription: blog.shortDescription || 'No short description available'
+          }));
+          console.log(this.allBlogs);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
   }
 
-  // create(data: Blog) {
-  //   this.blogsService.createBlogs(data);
-  // }
-  
+  // Function to open modal with blog details
+  openBlogModal(blog: Blog) {
+    this.selectedBlog = blog;
+  }
+
+  // Function to close modal
+  closeBlogModal() {
+    this.selectedBlog = null;
+  }
 }
